f06248c929ae363fb1a2667023146ebc83b71b0e
Lucas <lucrovader@gmail.com>
2023-02-09 18:52:01 -0300

    adiciona lista de herois e os torna clicaveis

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
-    name: 'Spider-man'
+    name: 'Caitlyn',
   };
 
-  constructor() {}
+  heroes = HEROES;
+  selectedHero?: Hero;
 
-  ngOnInit(): void {}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
