1488538d73a37adf3feeb144d18873b0ca17a981
Lucas <lucrovader@gmail.com>
2023-02-22 18:37:27 -0300

    add core

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { RouterModule } from '@angular/router';
+
+const COMPONENTS = [ToolbarComponent, MessagesComponent];
+const MODULES = [FlexLayoutModule, MaterialModule, RouterModule];
+
+@NgModule({
+  declarations: [COMPONENTS],
+  imports: [MODULES, CommonModule],
+  exports: [COMPONENTS, MODULES],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule has already been loaded. Import this module in the AppModule.'
+      );
+    }
+  }
+}
