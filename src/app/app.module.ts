1cac9935f501c3e0d745720ef9b099a36ce2307b
Lucas <lucrovader@gmail.com>
2023-02-22 14:53:29 -0300

    cria material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,11 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MessagesComponent } from './messages/messages.component';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { AppRoutingModule } from './app-routing.module';
-import { MatIconModule } from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -31,17 +23,9 @@ import { MatTableModule } from '@angular/material/table';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatExpansionModule,
     AppRoutingModule,
-    MatIconModule,
     FlexLayoutModule,
-    MatTooltipModule,
-    MatCardModule,
-    MatListModule,
-    MatInputModule,
-    MatTableModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+const MODULES = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatListModule,
+  MatInputModule,
+  MatTableModule,
+];
+
+@NgModule({
+  imports: [MODULES],
+  exports: [MODULES],
+})
+export class MaterialModule {}
